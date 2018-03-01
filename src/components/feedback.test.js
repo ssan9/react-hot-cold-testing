404b20fd81fd9fb4cf06ef5babcbb4b11e2b433e
ssan9 <perrsy@gmail.com>
2018-02-28 19:55:42 -0500

    removed errors

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -4,7 +4,7 @@ import {shallow} from 'enzyme';
 import Feedback from './feedback';
 
 describe('<Feedback', () => {
-	it('Renders without crashing'. () => {
+	it('Renders without crashing', () => {
 		shallow(<Feedback />);
 	});
 
